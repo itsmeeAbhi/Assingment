f2aa9d9769739d604124a6302dc64451bae46a7a
itsmeeAbhi <107909223+itsmeeAbhi@users.noreply.github.com>
2022-07-21 14:49:20 +0530

    Add files via upload

---
+++ Book Management System/bms.js
@@ -0,0 +1,53 @@
+document.querySelectorAll('.container img').forEach(image =>{
+    image.onclick =() => {
+        document.querySelector('.popup-image').style.display = 'block';
+        document.querySelector('.popup-image img').src = image.src;
+    }
+});
+
+document.querySelector('.popup-image span').onclick = () =>{
+    document.querySelector('.popup-image').style.display = 'none';
+}
+
+$(document).ready( function () {
+    $('#myTable').DataTable({
+        "lengthMenu": [ 5, 10, 15, 20 ]
+    });
+} );
+
+document.querySelectorAll(".carousel").forEach((carousel) => {
+    const items = carousel.querySelectorAll(".carousel__item");
+    const buttonsHtml = Array.from(items, () => {
+      return `<span class="carousel__button"></span>`;
+    });
+  
+    carousel.insertAdjacentHTML(
+      "beforeend",
+      `
+          <div class="carousel__nav">
+              ${buttonsHtml.join("")}
+          </div>
+      `
+    );
+  
+    const buttons = carousel.querySelectorAll(".carousel__button");
+  
+    buttons.forEach((button, i) => {
+      button.addEventListener("click", () => {
+        // un-select all the items
+        items.forEach((item) =>
+          item.classList.remove("carousel__item--selected")
+        );
+        buttons.forEach((button) =>
+          button.classList.remove("carousel__button--selected")
+        );
+  
+        items[i].classList.add("carousel__item--selected");
+        button.classList.add("carousel__button--selected");
+      });
+    });
+  
+    // Select the first item on page load
+    items[0].classList.add("carousel__item--selected");
+    buttons[0].classList.add("carousel__button--selected");
+  });
